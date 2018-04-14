39f30dc7aebccea426722f25a4545e8aae9c00aa
mahsa2017 <upmahjour@gmail.com>
2018-04-14 13:17:40 +0100

    added a bit temp eng

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const exphbs = require("express-handlebars");
 const express = require("express");
 const app = express();
+
 
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
@@ -9,4 +11,20 @@ app.use(express.static("public", {'extensions': ['html']}));
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+app.get("/contact-information",function(err,res){
+  res.send("welcome to contact me page");
+  
+})
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/my-cv", (req, res) => {
+  res.render("my-cv");
+});
+app.get("/admin", (req, res) => {
+  res.render("admin");
 });
