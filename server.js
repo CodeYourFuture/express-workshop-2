dffa94ee834c1d2c7d745a1bcb73c9805ab3712f
vikrep <protsenko.viktor@gmail.com>
2018-04-16 23:57:39 +0100

    Buttom GetRepoList works

--- server.js
+++ server.js
@@ -2,24 +2,31 @@ const exphbs = require("express-handlebars");
 const express = require("express");
 const app = express();
 const fs=require('fs');
+const filePath = __dirname + "/data/posts.json";
 
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
 
 // what does this line mean: process.env.PORT || 3000
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
-});
 
+app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// app.get("/", (req, res) => {
-//   res.render("index", {
-//     title: "Viktor Protsenko",     // insert your name instead
-//     subtitle: "Junior Wed developer"
-//   });
-// });
+app.get("/", (req, res) => {
+    const callbackFunction = (error, file) => {
+    // we call .toString() to turn the file buffer to a String
+    const fileData = file.toString();
+    // we use JSON.parse to get an object out the String
+    const postsJson = JSON.parse(fileData);
+    // send the json to the Template to render
+    res.render("index", {
+      title: "Mah/ViK Profile", // insert your name instead
+      posts: postsJson
+    });
+  };
+  fs.readFile(filePath, callbackFunction);
+});
 app.get("/my-cv", (req, res) => {
   res.render("my-cv", {
     title: "E-mail Me" // insert your name instead
@@ -33,30 +40,16 @@ app.get("/admin", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
-app.use(express.static("public", {'extensions': ['html']}));
-
-app.get("/", (req, res) => {
-  const filePath = __dirname + "/data/posts.json";
-  const callbackFunction = (error, file) => {
-    // we call .toString() to turn the file buffer to a String
-    const fileData = file.toString();
-    // we use JSON.parse to get an object out the String
-    const postsJson = JSON.parse(fileData);
-    // send the json to the Template to render
-    res.render("index", {
-      title: "Mah/ViK Profile", // insert your name instead
-      posts: postsJson
-    });
-  };
-  fs.readFile(filePath, callbackFunction);
-});
 
 //posts.json as an API
 app.get("/api/posts",(req,res)=> {
-  const filePath = __dirname + "/data/posts.json";
-  res.sendFile(filePath);
+    res.sendFile(filePath);
 
 })
 app.get('/posts/:postId', function (req, res) {
   res.send('post id: ' + req.params.postId);
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
 });
