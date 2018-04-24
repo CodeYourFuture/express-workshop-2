13fffc9e2d1be940e32e15d1dda14a4949186cb5
vikrep <protsenko.viktor@gmail.com>
2018-04-25 00:29:15 +0100

    Consume a posts API

--- public/js/apiajax.js
+++ public/js/apiajax.js
@@ -1,34 +1,26 @@
-var apiurl = "http://quotesondesign.com/wp-json/posts";
+var apiurl = "https://portfoliocyf.herokuapp.com/posts";
 var apiButton = document.querySelector("a.btn.btn-success");
-console.log(apiButton); //shows button
 
 apiButton.addEventListener("click", function (e) {
     e.preventDefault();
     var xhr = new XMLHttpRequest();
     xhr.open('GET', apiurl);
     xhr.onload = function (xhr) {
-        let html = '<div class="post-preview">'
-        reconstruct(this.responseText)
+        let html = '<div class="post-preview">';
+        let postInfo = JSON.parse(this.responseText);
+        let posts = post => {
+            html += `<a href="post.html">
+                <h2 class="post-id">${post.id}</h2>
+                <h2 class="post-title">${post.title}</h2>
+                <h3 class="post-summary">${post.summary}</h3>
+                <h3 class="post-content">${post.content}</h3>`;
+        }
+        postInfo.forEach(posts);
+        html += '</div>';
+        const postInfoElement = document.getElementById('postInfo');
+        postInfoElement.innerHTML = html;
     };
 
     xhr.setRequestHeader('Content-Type', 'application/json');
-    console.log(xhr.responseText)
     xhr.send();
-})
-function reconstruct(arg) {
-    let postInfo = this.responseText
-    let posts = post => {
-        html += `<a href="post.html">
-                <h2 class="post-title">${post.title}
-                </h2>
-        <h3 class= "post-subtitle">${post.content}
-        </h3>
-        </a>
-            <p class="post-meta"> Posted by ${post.link}
-                <a href="#"> </a>on july 8,2018 </p>`;
-    }
-    postInfo.forEach(posts);
-    html += '</div>';
-    const postInfoElement = document.getElementById('postInfo');
-    postInfoElement.innerHTML = html;
-}
+});
