abdcfaacb3b144ff63b8862a63ae84598f540a64
mahsa2017 <upmahjour@gmail.com>
2018-04-25 12:33:12 +0100

    nothing

--- public/js/apiajax.js
+++ public/js/apiajax.js
@@ -23,21 +23,4 @@ apiButton.addEventListener("click", function (e) {
 
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send();
-})
-function reconstruct(arg) {
-    let postInfo = this.responseText
-    let posts = post => {
-        html += `<a href="post.html">
-                <h2 class="post-title">${post.title}
-                </h2>
-        <h3 class= "post-subtitle">${post.content}
-        </h3>
-        </a>
-            <p class="post-meta"> Posted by ${post.link}
-                <a href="#"> </a> on july 8,2018 </p>`;
-    }
-    postInfo.forEach(posts);
-    html += '</div>';
-    const postInfoElement = document.getElementById('postInfo');
-    postInfoElement.innerHTML = html;
-}
+});
