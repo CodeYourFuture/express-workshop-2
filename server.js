c2096e7682159496b65363d2f15f53d63cf53602
Awet Fessha <awetkifle94@gmail.com>
2018-06-23 15:08:45 +0100

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
+// Then these two lines after you initialise your express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// The extensions 'html' allows us to serve file without adding .html at the end 
+// The extensions 'html' allows us to serve file without adding .html at the end
 // i.e /my-cv will server /my-cv.html
-app.use(express.static("public", {'extensions': ['html']}));
 
-
+app.get("/", function(req, res) {
+  res.render("index");
+});
+app.get("/my-cv", function(req, res) {
+  res.render("my-cv");
+});
+app.get("/admin", function(req, res) {
+  res.render("admin");
+});
+app.use(express.static("public", { extensions: ["html"] }));
 // what does this line mean: process.env.PORT || 3000
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
+app.listen(process.env.PORT || 3001, function() {
+  console.log("Server is listening on port 3001. Ready to accept requests!");
 });
