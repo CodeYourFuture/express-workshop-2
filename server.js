dd0a7a05337c5826b830b5614f6a7551958ee375
raj <rraju12@gmail.com>
2018-07-22 16:59:05 +0100

    Add solutions to part one

--- server.js
+++ server.js
@@ -5,14 +5,28 @@ const exphbs = require("express-handlebars");
 const app = express();
 // Then these two lines after you initialise your express app
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// exphbs.registerPartial(__dirname + "/views/partials");
+// registerPartial("myPartial", "{{name}}");
 app.set("view engine", "handlebars");
+
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
-app.use(express.static("public", {'extensions': ['html']}));
-
-app.get("/contact", (req, res) => {
-  res.send("This is a contact page");
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Raj profile" // insert your name instead
+  });
 });
+
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
+app.get("/my-cv", (req, res) => {
+  res.render("my-cv");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.use(express.static("public", { 'extensions': ['html'] }));
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
