bae2c1826076532a89cfda7e22a96c6cf2080609
Francisca Vargas <francisca@franciscavargas.me>
2018-07-22 16:28:26 +0100

    endpoint to return a simple string this is a contact page

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
-// The extensions 'html' allows us to serve file without adding .html at the end 
+// The extensions 'html' allows us to serve file without adding .html at the end
 // i.e /my-cv will server /my-cv.html
-app.use(express.static("public", {'extensions': ['html']}));
+app.use(express.static("public", { extensions: ["html"] }));
 
+app.get("/contact", function(req, res) {
+  res.send("Hello contact");
+});
 
 // what does this line mean: process.env.PORT || 3000
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
