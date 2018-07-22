4a8bebf8775403c48e2f744a998c16beeddcdccf
Francisca Vargas <francisca@franciscavargas.me>
2018-07-22 16:35:23 +0100

    added handlebars and point to public

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+// Add this to the top of your file
+const exphbs = require("express-handlebars");
 const express = require("express");
 const app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // The extensions 'html' allows us to serve file without adding .html at the end
 // i.e /my-cv will server /my-cv.html
 app.use(express.static("public", { extensions: ["html"] }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.get("/contact", function(req, res) {
   res.send("Hello contact");
